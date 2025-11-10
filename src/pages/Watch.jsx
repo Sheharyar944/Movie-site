e81c158484b04eb438c1167023a8f410c1e40ee8
shehayar944 <sheharyar.tariq44@gmail.com>
2025-11-11 02:43:52 +0500

    Changed video urls

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -515,7 +515,7 @@ const Watch = () => {
         >
           {trailer ? (
             <ReactPlayer
-              url={`https://www.youtube.com/embed/${trailer}`}
+              url={`https://www.youtube.com/embed/ZcQ_L_u0Kgo`}
               width="1260px"
               height="640px"
               controls={true}
@@ -752,7 +752,7 @@ const Watch = () => {
               )}
               {trailer ? (
                 <ReactPlayer
-                  url={`https://www.youtube.com/embed/${trailer}`}
+                  url={`https://www.youtube.com/embed/ZcQ_L_u0Kgo`}
                   width="100%"
                   height="100%"
                   controls={true}
